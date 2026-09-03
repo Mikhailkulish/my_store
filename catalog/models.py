1ac8c420854fe8da5a3d3583b08f3fa438dbe125
Михаил Кулиш <mkulis704@gmal.com>
2026-09-03 19:45:03 +0300

    fixed catalog.models.py

--- catalog/models.py
+++ catalog/models.py
@@ -2,31 +2,65 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
+    title = models.CharField(
+        max_length=100, verbose_name="Товар", help_text="Введите наименование товара"
+    )
     description = models.TextField()
-    image = models.ImageField(upload_to='images/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    photo = models.ImageField(
+        upload_to="products/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото товара",
+        help_text="Загрузите фото товара",
+    )
+    category = models.ForeignKey(
+        on_delete=models.SET_NULL,
+        help_text="Введите категорию товара",
+        blank=True,
+        null=True,
+        related_name="products",
+    )
     purchase_price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(
+        auto_now_add=True,
+        blank=True,
+        null=True,
+        verbose_name="Дата создания товара",
+        help_text="Укажите дату создания товара",
+    )
+    updated_at = models.DateField(
+        auto_now_add=True,
+        blank=True,
+        null=True,
+        verbose_name="Дата изменения товара",
+        help_text="Укажите дату изменения товара",
+    )
 
     def __str__(self):
-        return f'{self.title}'
+        return self.title
 
     class Meta:
-        verbose_name = 'наименование'
-        verbose_name_plural = 'наименования'
-        ordering = ['title']
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["category", "title"]
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField()
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
-        return f'{self.title}'
+        return self.title
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
-        ordering = ['title']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"

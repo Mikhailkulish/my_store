146cb9b6cd28637a01e7cf661650759e9710d8a6
Михаил Кулиш <mkulis704@gmal.com>
2026-09-04 16:57:14 +0300

    fixed catalog.admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", )
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")

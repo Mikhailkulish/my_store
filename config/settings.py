f6443e817423392d82fcea7b631d0263b76aa197
Михаил Кулиш <mkulis704@gmal.com>
2026-09-06 16:39:22 +0300

    add media

--- config/settings.py
+++ config/settings.py
@@ -97,3 +97,9 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
